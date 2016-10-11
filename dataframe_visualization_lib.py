98e0eead3c372de3684cabb2ed847ed134215661
Christopher Shymansky <cmshymansky@gmail.com>
2016-10-11 12:56:29 -0700

    Started a feature branch a bit early

--- dataframe_visualization_lib.py
+++ dataframe_visualization_lib.py
@@ -4,50 +4,35 @@ import inspect
 import matplotlib
 import matplotlib.pyplot as plt
 
-def plot_label_versus_label(input_label_feature,output_label_feature,output_labels=[],
-                            colors=[],alpha=0.6,figure_size=[12,6],x_label=[],title=[]):
-    # Form automatic title if not provided
-    if not title:
-        title = 'Proportations of '+output_label_feature+' category within '+input_label_feature+' populations'
-
-    # Set font attributes
-    large_text_size = 14
-    small_text_size = 12
-
-    # Set axis/legend labels
-    y_label = input_label_feature
-    if not x_label:
-        x_label = 'Frequency'
-        
-    # Form pivot table between input and output label features
-    label_by_label = df[[input_label_feature,output_label_feature]].pivot_table(columns=[output_label_feature],index=[input_label_feature],aggfunc=len)
-
-    # Plot pivot table as stacked column chart
-    label_by_label.plot(kind='barh',stacked=True,color=colors,alpha=alpha,figsize=figure_size)
-
-    # Set title, x and y labels, and legend values
-    plt.title(title,size=large_text_size)
-
-    plt.xlabel(x_label,size=small_text_size)
-    plt.ylabel(y_label,size=small_text_size)
-
-    if output_labels:
-        legend_labels = output_labels
-        plt.legend(legend_labels,loc='best')
-    else:
-        plt.legend(loc='best')
-        
-    plt.show()
+def compare_data(df,plot_vars=[]):
+    """
+    Outputs a modified pair grid where data plots takes this form:
     
-# input_label_feature = 'Sex'
-# output_label_feature = 'Survived'
-# output_labels = ['Died','Survived']
-# colors = ['grey','orange','red']
-# x_label = 'Number of passengers'
-
-# plot_label_versus_label(input_label_feature,output_label_feature,output_labels=output_labels,
-#                             colors=colors,x_label=x_label)
-
+    same feature vs same feature: Uncolored distribution
+    continuous vs continuous: scatter plot colored by the default feature
+    category vs continuous: distribution of continuous feature colored by category
+    category 1 vs category 2: distribution of category 2 colored by category 1
+    continuous vs category: As yet undetermined... probably blank
+    """
+    # Use all features if not explicitly provided by user
+    if not plot_vars:
+        plot_vars = list(df.columns)
+    
+    # Count number of features
+    
+    
+    
+    # Obtain feature types
+    
+    # Form blank pairgrid
+    
+    # Iterate through each block
+        # Figure out what type of plot to make based on features
+        # Create plot for that position
+        # Add legend (possibly in the plot or far to the right or left)
+    
+    pass
+    
 def continuous_pair_grid_vs_label(df,plot_vars=[],hue_feature=[],scatter_alpha=0.2,
                                   bar_alpha=0.3,num_bars=20,scatter_size=45,
                                   palette=['grey','orange','red'],fig_size=6,fig_aspect=1,
@@ -98,3 +83,38 @@ def continuous_pair_grid_vs_label(df,plot_vars=[],hue_feature=[],scatter_alpha=0
     # Add legend if there is a hue feature
     if hue_feature:
         g.add_legend();
+        
+def plot_label_versus_label(df,input_label_feature,output_label_feature,output_labels=[],
+                            colors=[],alpha=0.6,figure_size=[12,6],x_label=[],title=[]):
+    # Form automatic title if not provided
+    if not title:
+        title = 'Proportations of '+output_label_feature+' category within '+input_label_feature+' populations'
+
+    # Set font attributes
+    large_text_size = 14
+    small_text_size = 12
+
+    # Set axis/legend labels
+    y_label = input_label_feature
+    if not x_label:
+        x_label = 'Frequency'
+        
+    # Form pivot table between input and output label features
+    label_by_label = df[[input_label_feature,output_label_feature]].pivot_table(columns=[output_label_feature],index=[input_label_feature],aggfunc=len)
+
+    # Plot pivot table as stacked column chart
+    label_by_label.plot(kind='barh',stacked=True,color=colors,alpha=alpha,figsize=figure_size)
+
+    # Set title, x and y labels, and legend values
+    plt.title(title,size=large_text_size)
+
+    plt.xlabel(x_label,size=small_text_size)
+    plt.ylabel(y_label,size=small_text_size)
+
+    if output_labels:
+        legend_labels = output_labels
+        plt.legend(legend_labels,loc='best')
+    else:
+        plt.legend(loc='best')
+        
+    plt.show()
